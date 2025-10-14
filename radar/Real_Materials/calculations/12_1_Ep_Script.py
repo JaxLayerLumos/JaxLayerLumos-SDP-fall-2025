7c2e9b77b90a0ddb9faac802b3196b0eb8cbc3bb
jca64 <jca64@pitt.edu>
2025-10-14 19:37:58 -0400

    Reworking code for static database

---
+++ radar/Real_Materials/calculations/12_1_Ep_Script.py
@@ -0,0 +1,71 @@
+#This code models the frequency-dependent dielectric BerylliumOxide
+#Computes Permittivity, across the range 0.2 to 8 GHz
+#Inputs: B_ep1, C_ep1, D_ep1, G_ep1, H_ep1, I_ep1, and J_ep1, f_min
+#Outputs: .csv file '12.1_BerylliumOxide.csv'
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pdfplumber
+from Materials_Library import materials_data
+
+def getEpAndMu_12_1(user_f_min, user_f_max, material):
+    # Get frequency range 
+    f_min, f_max = material['freq_range_ghz']
+    if f_min is None or f_max is None:
+        raise ValueError(f"Could not find frequency range for {material['name']}")
+    
+    if user_f_min < f_min or user_f_max > f_max:
+        print(f"\n\nWARNING: Frequency range {f_min}–{f_max} GHz is defined for {material['name']}. All data outside of this range will be extrapolated.")
+    rows = []
+
+
+    B = material['eps_params']['B']
+    C = material['eps_params']['C']
+    D = material['eps_params']['D']
+    G = material['eps_params']['G']
+    H = material['eps_params']['H']
+    I = material['eps_params']['I']
+    J = material['eps_params']['J']
+    
+    num_points = 100 - 1  # Number of frequency points
+   
+    frequencies = np.linspace(user_f_min, user_f_max, num_points)
+    epsilon_f = B + 2 * C * (frequencies ** D) + G * (1 - J * (frequencies - H)**2 - 1j * 2 * I * frequencies)**(-1)
+
+
+    #permeability (mu = 1 for non-farreous)
+    mu_f = np.ones(frequencies.shape)
+
+    # loglog plot
+    plt.figure()
+    plt.loglog(frequencies, np.real(epsilon_f), 'b-', linewidth=2, label='Re($\epsilon$)')
+    plt.loglog(frequencies, np.imag(epsilon_f), 'r--', linewidth=2, label='Im($\epsilon$)')
+    plt.xlabel('Frequency [GHz]', fontsize=12)
+    plt.ylabel('Epsilon', fontsize=12)
+    plt.legend(loc='best')
+    plt.grid(True, which="both", ls="--")
+    plt.title('Real and Imaginary Permittivity vs. Frequency', fontsize=14)
+    plt.xlim(1e-1, 1e3)
+    plt.ylim(1e-4, 10)
+    plt.show()
+
+def main():
+    
+    i = 0
+     
+    for material in materials_data:
+        print(i, ". ", material['name'])
+        i+=1
+    
+    print('/n/n')
+    mat_idx = int(input("Please select a material index from the list below: "))
+     
+    freq_min = float(input("Input min freq: "))
+    freq_max = float(input("Input max freq: "))
+     
+    material = materials_data[mat_idx]
+    getEpAndMu_12_1(freq_min, freq_max, material)
+
+if __name__ == "__main__":
+    main()
