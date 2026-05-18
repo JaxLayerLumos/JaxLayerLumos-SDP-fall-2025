3e846c106c23796af9844bde4a7765095740a0d3
Paul Leu <pleu@pitt.edu>
2026-05-18 09:48:40 -0400

    updates

--- radar/Real_Materials/Optimization/LF_GeneticAlg_Real.py
+++ radar/Real_Materials/Optimization/LF_GeneticAlg_Real.py
@@ -22,7 +22,7 @@ freq_lowerbound = 0.2*10**9 #Hz
 freq_upperbound = 2*10**9 #Hz
 
 #Hyper-Parameters
-num_generations = 50 #Number of GA's to converge CHANGE THIS TO A HIGHER VALUE AFTER TESTING ~4000+
+num_generations = 3 #Number of GA's to converge CHANGE THIS TO A HIGHER VALUE AFTER TESTING ~4000+
 num_parents_mating = 6 #
 sol_per_pop = 40 #40 candidate designs for generation
 num_genes = 10 #Total decisions variables for individual, 5 materials and 2 for each
@@ -32,6 +32,7 @@ mutation_adaptive = (0.3, 0.05) #PyGAD’s adaptive mutation, where mutation pro
 
 # Define the gene space
 #Code defines domain for each of 10 gense
+
 gene_space = [{'low': 1, 'high': 13}] * layers + [{'low': 0.0003, 'high': 0.0015}] * layers 
 
 
